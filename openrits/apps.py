f2d349d6f9ec6b78103ff81a287346b31b228a64
jjkasprzak <jjkasprzak@protonmail.com>
2023-02-06 15:47:17 +0100

    Add django app openrits

---
+++ openrits/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OpenritsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "openrits"
+    verbose_name = "OpenRITS"
