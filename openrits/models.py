481485dede83ccb84d1fe40e142425715a0433e4
Jakub Kasprzak <73919276+jjkasprzak@users.noreply.github.com>
2023-02-21 01:43:48 +0100

    Add models (#1)

--- openrits/models.py
+++ openrits/models.py
@@ -1,1 +1,322 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.db.models.functions import Substr, StrIndex, Concat, Chr
+from django.db.models.query import QuerySet
+from django.db.models import Value as V, F, Q, Sum
+import datetime
+
+
+class PropertyDefinition(models.Model):
+    SUPPORTED_FIELDS = (
+        models.IntegerField,
+        models.FloatField,
+        models.BooleanField,
+        models.TextField,
+        models.DateField,
+    )
+
+    name = models.CharField(max_length=127)
+    property_type = models.CharField(
+        max_length=63,
+        choices=list((t.__name__, t.__name__) for t in SUPPORTED_FIELDS),
+    )
+
+    class Meta:
+        abstract = True
+
+
+class PropertyValue(models.Model):
+    TYPE_DICT = {t.__name__: t for t in PropertyDefinition.SUPPORTED_FIELDS}
+
+    value = models.TextField(default="")
+
+    def getPropertyType(self) -> str:
+        """
+        Subclasses should override this and return
+        their respective property type as str.
+        """
+        raise NotImplementedError()
+
+    def deserialize(self) -> any:
+        """
+        Deserializes value using type returned by getProperty.
+        """
+        property_type = self.getPropertyType()
+        return PropertyValue.TYPE_DICT[property_type]().to_python(self.value)
+
+    def serialize(self, object: any):
+        """
+        Serializes object and writes it to the value.
+        """
+        self.value = str(object)
+
+    class Meta:
+        abstract = True
+
+
+class ItemCategory(models.Model):
+    name = models.CharField(max_length=127)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    lineage = models.TextField(
+        editable=False,
+        default=",",
+        validators=[
+            RegexValidator(regex=r"^,(?:\d+,)*\Z", message="Incorrect lineage format")
+        ],
+    )
+
+    class Manager(models.Manager):
+        def filter_descendants(
+            self, category: "ItemCategory"
+        ) -> "QuerySet[ItemCategory]":
+            """
+            Return all subcategories for given category.
+            """
+            return self.filter(lineage__contains=f",{category.pk},")
+
+        def filter_ancestors(
+            self, category: "ItemCategory"
+        ) -> "QuerySet[ItemCategory]":
+            """
+            Return all supercategories for given category.
+            """
+            return self.alias(
+                chrpk=Concat(V(","), F("id"), V(","), output_field=models.CharField()),
+                ancestors=V(category.lineage),
+            ).filter(ancestors__contains=F("chrpk"))
+
+        def update_parent(
+            self, category: "ItemCategory", new_parent: "ItemCategory"
+        ) -> None:
+            """
+            Update parent category and adjust
+            all subcategories accordingly.
+            """
+            new_lineage = ","
+            new_parent_id = None
+            if new_parent is not None:
+                new_lineage = new_parent.lineage + f"{new_parent.pk},"
+                new_parent_id = new_parent.pk
+                check_qs = self.filter_descendants(category).filter(pk=new_parent_id)
+                if new_parent_id == category.pk:
+                    raise ValueError("Category can not reference itself as its parent.")
+                if check_qs:
+                    raise ValueError("New category parent must not be its descendant.")
+            self.filter(pk=category.pk).update(
+                parent=new_parent_id, lineage=new_lineage
+            )
+            descendantsLineage = new_lineage + f"{category.pk},"
+            searchStr = f",{category.pk},"
+            self.filter_descendants(category).update(
+                lineage=Concat(
+                    V(descendantsLineage),
+                    Substr(
+                        F("lineage"),
+                        StrIndex(F("lineage"), V(searchStr)) + len(searchStr),
+                    ),
+                )
+            )
+
+    objects = Manager()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=~Q(parent_id=F("id")), name="id_not_equal_parent"
+            ),
+            models.CheckConstraint(
+                check=~Q(lineage__contains=Concat(V(","), F("id"), V(","))),
+                name="id_not_in_lineage",
+            ),
+        ]
+
+
+class ItemCategoryProperty(PropertyDefinition):
+    category = models.ForeignKey(ItemCategory, on_delete=models.CASCADE)
+
+    class Manager(models.Manager):
+        def filter_relevant_for(
+            self, category: ItemCategory
+        ) -> "QuerySet[ItemCategoryProperty]":
+            """
+            Return ItemCategoryProperties for given category and all supercategories.
+            """
+            return self.alias(
+                chrpk=Concat(
+                    V(","), "category_id", V(","), output_field=models.CharField()
+                ),
+                categories=Concat(
+                    V(category.lineage),
+                    V(category.pk),
+                    V(","),
+                    output_field=models.CharField(),
+                ),
+            ).filter(categories__contains=F("chrpk"))
+
+    objects = Manager()
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=127)
+    amount = models.IntegerField(default=0)
+    archived = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        ItemCategory, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    class Manager(models.Manager):
+        def get_available_amount(
+            self, item: "Item", start: datetime.datetime, end: datetime.datetime
+        ) -> int:
+            """
+            Return amount of available item for given time.
+            """
+            if item.amount <= 0:
+                return 0
+
+            coliding_rents = (
+                Rent.objects.filter(start__lte=end, end__gte=start)
+                .filter(rentitem__item=item)
+                .annotate(
+                    amount=Sum(F("rentitem__amount"), filter=Q(rentitem__item=item))
+                )
+            )
+            if not coliding_rents:
+                return item.amount
+
+            points_of_interest = []
+            for rent in coliding_rents:
+                points_of_interest.append((rent.start, rent.amount))
+                points_of_interest.append((rent.end, -rent.amount))
+            points_of_interest = sorted(points_of_interest, key=lambda e: e[0])
+
+            storage_amount = item.amount
+            min_amount = storage_amount
+            for poi in points_of_interest:
+                storage_amount -= poi[1]
+                min_amount = min(min_amount, storage_amount)
+            return min_amount
+
+    objects = Manager()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=Q(amount__gte=0), name="item_amount_gte_0"),
+        ]
+
+
+class ItemPropertyValue(PropertyValue):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    property = models.ForeignKey(ItemCategoryProperty, on_delete=models.CASCADE)
+
+    class Manager(models.Manager):
+        def filter_relevant_for(self, item: Item) -> "QuerySet[ItemPropertyValue]":
+            """
+            Return values for given item and its property definitions.
+            """
+            defined_properties = ItemCategoryProperty.objects.filter_relevant_for(
+                item.category
+            ).only("id")
+            return self.filter(item=item).filter(property__in=defined_properties)
+
+        def filter_obsolete_for(self, item: Item) -> "QuerySet[ItemPropertyValue]":
+            """
+            Return values for given item that are not related to
+            any of item current property definitions.
+            """
+            defined_properties = ItemCategoryProperty.objects.filter_relevant_for(
+                item.category
+            ).only("id")
+            return self.filter(item=item).exclude(property__in=defined_properties)
+
+    objects = Manager()
+
+    def getPropertyType(self) -> str:
+        return self.property.property_type
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=("item", "property"), name="unique_item_property"
+            ),
+        ]
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=127)
+    surname = models.CharField(max_length=127)
+    email = models.EmailField()
+
+
+class CustomerProperty(PropertyDefinition):
+    pass
+
+
+class CustomerPropertyValue(PropertyValue):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    property = models.ForeignKey(CustomerProperty, on_delete=models.CASCADE)
+
+    def getPropertyType(self) -> str:
+        return self.property.property_type
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=("customer", "property"), name="unique_customer_property"
+            ),
+        ]
+
+
+class Rent(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    issued = models.DateTimeField(null=True, blank=True)
+    returned = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=Q(start__lte=F("end")), name="start_lte_end"),
+            models.CheckConstraint(
+                check=Q(issued__isnull=True)
+                | Q(returned__isnull=True)
+                | Q(issued__lte=F("returned")),
+                name="issued_lte_returned",
+            ),
+            models.CheckConstraint(
+                check=~Q(Q(issued__isnull=True) & Q(returned__isnull=False)),
+                name="returned_but_not_issued",
+            ),
+        ]
+
+
+class RentProperty(PropertyDefinition):
+    pass
+
+
+class RentPropertyValue(PropertyValue):
+    rent = models.ForeignKey(Rent, on_delete=models.CASCADE)
+    property = models.ForeignKey(RentProperty, on_delete=models.CASCADE)
+
+    def getPropertyType(self) -> str:
+        return self.property.property_type
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=("rent", "property"), name="unique_rent_property"
+            )
+        ]
+
+
+class RentItem(models.Model):
+    amount = models.IntegerField()
+    item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
+    rent = models.ForeignKey(Rent, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=Q(amount__gt=0), name="rent_amount_gt_0"),
+            models.UniqueConstraint(fields=("item", "rent"), name="unique_item_rent"),
+        ]
--- openrits/tests.py
+++ openrits/tests.py
@@ -1,1 +1,313 @@
 from django.test import TestCase
+
+from .models import (
+    PropertyDefinition,
+    Item,
+    ItemPropertyValue,
+    ItemCategoryProperty,
+    ItemCategory,
+    Rent,
+    RentItem,
+)
+from django.utils import timezone
+import datetime
+
+
+class ItemPropertyValue_ModelTests(TestCase):
+    def setUp(self):
+        a = ItemCategory.objects.create(name="A")
+        a_1 = ItemCategory.objects.create(name="A_1", parent=a, lineage=f",{a.pk},")
+        a_1_1 = ItemCategory.objects.create(
+            name="A_1_1", parent=a_1, lineage=f"{a_1.lineage}{a_1.pk},"
+        )
+        cat_b = ItemCategory.objects.create(name="B")
+
+        a_thing = Item.objects.create(name="a_thing", category=a)
+        a_1_thing = Item.objects.create(name="a_1_thing", category=a_1)
+
+        for cat in (a, a_1, a_1_1):
+            prop_def = ItemCategoryProperty.objects.create(
+                name=cat.name + "_prop", property_type="IntegerField", category=cat
+            )
+            ItemPropertyValue.objects.create(value="1", item=a_thing, property=prop_def)
+            ItemPropertyValue.objects.create(
+                value="2", item=a_1_thing, property=prop_def
+            )
+
+        b_thing = Item.objects.create(name="b_thing", category=cat_b)
+        for field in PropertyDefinition.SUPPORTED_FIELDS:
+            prop = ItemCategoryProperty.objects.create(
+                name=field.__name__, property_type=field.__name__, category=cat_b
+            )
+            ItemPropertyValue.objects.create(item=b_thing, property=prop)
+
+    SERIALIZATION_CASES = [
+        ("IntegerField", "1", 1),
+        ("FloatField", "0.5", 0.5),
+        ("BooleanField", "True", True),
+        ("TextField", "Hello sir!", "Hello sir!"),
+        (
+            "DateField",
+            str(datetime.date(1918, 11, 11)),
+            datetime.date(1918, 11, 11),
+        ),
+    ]
+
+    def test_deserialize(self):
+        fields_to_check = set(t.__name__ for t in PropertyDefinition.SUPPORTED_FIELDS)
+        for field, serialized, deserialized in self.SERIALIZATION_CASES:
+            self.assertTrue(
+                field in fields_to_check,
+                f"Field {field} not supported or already tested",
+            )
+            value = ItemCategoryProperty.objects.get(
+                name=field
+            ).itempropertyvalue_set.first()
+            value.value = serialized
+            result = value.deserialize()
+            self.assertEqual(
+                result,
+                deserialized,
+                f"{field} deserialize - expected {deserialized}, but got {result}",
+            )
+            fields_to_check.remove(field)
+        self.assertEqual(
+            len(fields_to_check),
+            0,
+            f"Some of supported fields were omitted {fields_to_check}",
+        )
+
+    def test_serialize(self):
+        fields_to_check = set(t.__name__ for t in PropertyDefinition.SUPPORTED_FIELDS)
+        for field, serialized, deserialized in self.SERIALIZATION_CASES:
+            self.assertTrue(
+                field in fields_to_check,
+                f"Field {field} not supported or already tested",
+            )
+            value = ItemCategoryProperty.objects.get(
+                name=field
+            ).itempropertyvalue_set.first()
+            value.serialize(deserialized)
+            self.assertEqual(
+                value.value,
+                serialized,
+                f"{field} serialize - expected {serialized}, but got {value.value}",
+            )
+            fields_to_check.remove(field)
+        self.assertEqual(
+            len(fields_to_check),
+            0,
+            f"Some of supported fields were omitted {fields_to_check}",
+        )
+
+    def test_filter_relevant_for(self):
+        thing1 = Item.objects.get(name="a_1_thing")
+
+        thing1_values = ItemPropertyValue.objects.filter_relevant_for(thing1)
+        result = list((v.property.name, v.value) for v in thing1_values)
+        expected = [("A_prop", "2"), ("A_1_prop", "2")]
+
+        self.assertEqual(result, expected, f"Expected {expected}, but got {result}")
+
+    def test_filter_obsolete_for(self):
+        thing1 = Item.objects.get(name="a_1_thing")
+
+        thing1_values = ItemPropertyValue.objects.filter_obsolete_for(thing1)
+        result = list((v.property.name, v.value) for v in thing1_values)
+        expected = [("A_1_1_prop", "2")]
+
+        self.assertEqual(result, expected, f"Expected {expected}, but got {result}")
+
+
+class ItemCategory_ModelTests(TestCase):
+    def setUp(self):
+        cat_a = ItemCategory.objects.create(name="A")
+        cat_a_1 = ItemCategory.objects.create(
+            name="A_1", parent=cat_a, lineage=f",{cat_a.pk},"
+        )
+        cat_a_1_1 = ItemCategory.objects.create(
+            name="A_1_1", parent=cat_a_1, lineage=f"{cat_a_1.lineage}{cat_a_1.pk},"
+        )
+        ItemCategory.objects.create(
+            name="A_1_1_1",
+            parent=cat_a_1_1,
+            lineage=f"{cat_a_1_1.lineage}{cat_a_1_1.pk},",
+        )
+        ItemCategory.objects.create(name="A_2", parent=cat_a, lineage=f",{cat_a.pk},")
+        ItemCategory.objects.create(name="B")
+
+    def test_filter_descendants(self):
+        cat_a = ItemCategory.objects.get(name="A")
+        descendants = ItemCategory.objects.filter_descendants(cat_a)
+
+        names = set(d.name for d in descendants)
+        expected = set(["A_1", "A_2", "A_1_1", "A_1_1_1"])
+
+        self.assertEqual(names, expected, f"Expected {expected}, but got {names}")
+
+    def test_filter_descendants_no_descendants(self):
+        cat_b = ItemCategory.objects.get(name="B")
+        descendants = ItemCategory.objects.filter_descendants(cat_b)
+
+        names = set(d.name for d in descendants)
+        expected = set()
+
+        self.assertEqual(names, expected, f"Expected {expected}, but got {names}")
+
+    def test_update_parent_to_other(self):
+        b = ItemCategory.objects.get(name="B")
+        a_1 = ItemCategory.objects.get(name="A_1")
+        a_1_1 = ItemCategory.objects.get(name="A_1_1")
+        a_1_2 = ItemCategory.objects.get(name="A_1_1_1")
+        ItemCategory.objects.update_parent(a_1, b)
+
+        expected_parent = b.pk
+        expected_lineage = f",{b.pk},"
+        for cat in (a_1, a_1_1, a_1_2):
+            cat.refresh_from_db()
+            self.assertEqual(
+                cat.parent_id, expected_parent, f"Incorrect parent of {cat.name}"
+            )
+            self.assertEqual(
+                cat.lineage, expected_lineage, f"Incorrect lineage of {cat.name}"
+            )
+            expected_parent = cat.pk
+            expected_lineage += f"{cat.pk},"
+
+    def test_update_parent_to_null(self):
+        a_1 = ItemCategory.objects.get(name="A_1")
+        a_1_1 = ItemCategory.objects.get(name="A_1_1")
+        a_1_1_1 = ItemCategory.objects.get(name="A_1_1_1")
+        ItemCategory.objects.update_parent(a_1, None)
+
+        expected_parent = None
+        expected_lineage = f","
+        for cat in (a_1, a_1_1, a_1_1_1):
+            cat.refresh_from_db()
+            self.assertEqual(
+                cat.parent_id, expected_parent, f"Incorrect parent of {cat.name}"
+            )
+            self.assertEqual(
+                cat.lineage, expected_lineage, f"Incorrect lineage of {cat.name}"
+            )
+            expected_parent = cat.pk
+            expected_lineage += f"{cat.pk},"
+
+    def test_update_parent_to_descendant(self):
+        a_1 = ItemCategory.objects.get(name="A_1")
+        a_1_1_1 = ItemCategory.objects.get(name="A_1_1_1")
+        with self.assertRaises(ValueError):
+            ItemCategory.objects.update_parent(a_1, a_1_1_1)
+
+    def test_update_parent_to_self(self):
+        a_1 = ItemCategory.objects.get(name="A_1")
+        with self.assertRaises(ValueError):
+            ItemCategory.objects.update_parent(a_1, a_1)
+
+    def test_filter_ancestors(self):
+        a_1_1 = ItemCategory.objects.get(name="A_1_1")
+        ancestors = ItemCategory.objects.filter_ancestors(a_1_1)
+
+        names = list(ancestor.name for ancestor in ancestors)
+        expected = ["A", "A_1"]
+
+        self.assertEqual(names, expected, f"Expected {expected}, but got {names}")
+
+    def test_filter_ancestors_no_ancestors(self):
+        b = ItemCategory.objects.get(name="B")
+        ancestors = ItemCategory.objects.filter_ancestors(b)
+
+        names = list(ancestor.name for ancestor in ancestors)
+        expected = []
+
+        self.assertEqual(names, expected, f"Expected {expected}, but got {names}")
+
+
+class ItemCategoryProperty_ModelTests(TestCase):
+    def setUp(self):
+        cat_a = ItemCategory.objects.create(name="A")
+        cat_a_1 = ItemCategory.objects.create(
+            name="A_1", parent=cat_a, lineage=f",{cat_a.pk},"
+        )
+        cat_a_1_1 = ItemCategory.objects.create(
+            name="A_1_1", parent=cat_a_1, lineage=f"{cat_a_1.lineage}{cat_a_1.pk},"
+        )
+        ItemCategory.objects.create(
+            name="A_1_1_1",
+            parent=cat_a_1_1,
+            lineage=f"{cat_a_1_1.lineage}{cat_a_1_1.pk},",
+        )
+        ItemCategory.objects.create(name="A_2", parent=cat_a, lineage=f",{cat_a.pk},")
+        for cat in ItemCategory.objects.all():
+            ItemCategoryProperty.objects.create(
+                name=cat.name + "_prop", property_type="IntegerField", category=cat
+            )
+        ItemCategory.objects.create(name="B")
+
+    def test_filter_relevant_for(self):
+        a_1_1 = ItemCategory.objects.get(name="A_1_1")
+        properties = ItemCategoryProperty.objects.filter_relevant_for(a_1_1)
+
+        names = list(prop.name for prop in properties)
+        expected = ["A_prop", "A_1_prop", "A_1_1_prop"]
+
+        self.assertEqual(names, expected, f"Expected {expected}, but got {names}")
+
+    def test_filter_relevant_for_no_properties(self):
+        b = ItemCategory.objects.get(name="B")
+        properties = ItemCategoryProperty.objects.filter_relevant_for(b)
+
+        names = list(prop.name for prop in properties)
+        expected = []
+
+        self.assertEqual(names, expected, f"Expected {expected}, but got {names}")
+
+
+class Item_ModelTests(TestCase):
+    def setUp(self):
+        a = ItemCategory.objects.create(name="A")
+        thing1 = Item.objects.create(name="thing1", category=a, amount=8)
+        thing2 = Item.objects.create(name="thing2", category=a, amount=10)
+        now = timezone.now()
+        tdelta = datetime.timedelta
+        surrounding_rent = Rent.objects.create(
+            created=now, start=now, end=now + tdelta(days=4)
+        )
+        left_rent = Rent.objects.create(
+            created=now, start=now, end=now + tdelta(days=2, hours=12)
+        )
+        for _ in range(3):
+            small_left_rent = Rent.objects.create(
+                created=now, start=now, end=now + tdelta(hours=12)
+            )
+        right_rent = Rent.objects.create(
+            created=now, start=now + tdelta(days=1, hours=12), end=now + tdelta(days=4)
+        )
+        middle_rent = Rent.objects.create(
+            created=now,
+            start=now + tdelta(days=1, hours=23),
+            end=now + tdelta(days=2, hours=1),
+        )
+        for rent in Rent.objects.all():
+            RentItem.objects.create(item=thing1, rent=rent, amount=2)
+            RentItem.objects.create(item=thing2, rent=rent, amount=2)
+
+    def test_get_available_amount(self):
+        now = timezone.now()
+        tdelta = datetime.timedelta
+        thing1 = Item.objects.get(name="thing1")
+
+        amount = Item.objects.get_available_amount(
+            thing1, now + tdelta(days=1), now + tdelta(days=3)
+        )
+        self.assertEqual(amount, 0)
+
+        amount = Item.objects.get_available_amount(
+            thing1, now + tdelta(hours=11), now + tdelta(days=3)
+        )
+        self.assertEqual(amount, -2)
+
+        amount = Item.objects.get_available_amount(
+            thing1, now + tdelta(days=2, hours=2), now + tdelta(days=3)
+        )
+        self.assertEqual(amount, 2)
